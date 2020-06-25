e0c5ce5842b12a96fab81c4667733d068df6c4dd
Angel788 <angel-yahel@outlook.com>
2020-06-25 17:51:30 -0500

    Se agrego un documento javascript de forma typada

---
+++ hola.js
@@ -0,0 +1,13 @@
+var esacuaman = true;
+console.log(esacuaman);
+var rtu = /** @class */ (function () {
+    function rtu() {
+    }
+    rtu.prototype.gt4 = function (fyty) {
+        return fyty;
+    };
+    rtu.prototype.imprimir = function () {
+        console.log(this.gt4("Hola"));
+    };
+    return rtu;
+}());
---
+++ hola.ts
@@ -0,0 +1,10 @@
+let esacuaman:boolean=true;
+console.log(esacuaman);
+class rtu{
+     gt4(fyty:string){
+        return fyty;
+    }
+    imprimir(){
+        console.log(this.gt4("Hola"));
+    }
+}
